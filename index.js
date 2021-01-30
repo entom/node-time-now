1d765f68edd80692c14d254494acf9532fe0125f
entom <tomasz@entom.info>
2021-01-30 20:39:25 +0100

    feat(app): initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+exports.now = (unit = 'nano') => {
+    let time = null;
+    const processHrTime = process.hrtime();
+    switch (unit) {
+        case 'milli':
+            time = processHrTime[0] * 1000 + processHrTime[1] / 1000000;
+        case 'micro':
+            time = hrTime[0] * 1000000 + processHrTime[1] / 1000;
+        case 'nano':
+            time = hrTime[0] * 1000000000 + processHrTime[1];
+    }
+
+    return time;
+}
