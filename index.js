ff045a6f06ff92616cc405c28018e74af8549444
entom <tomasz@entom.info>
2021-01-31 09:52:47 +0100

    feat(app): add break into switch statement, add jshintrc file

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ exports.now = (unit = 'nano') => {
     switch (unit) {
         case 'milli':
             time = processHrTime[0] * 1000 + processHrTime[1] / 1000000;
+            break;
         case 'micro':
             time = processHrTime[0] * 1000000 + processHrTime[1] / 1000;
+            break;
         case 'nano':
             time = processHrTime[0] * 1000000000 + processHrTime[1];
+            break;
     }
 
     return time;
